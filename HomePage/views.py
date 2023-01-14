31ae0b3758224ad4b7016e6b5a44e1c6dd070273
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-01-14 02:19:26 +0200

    Modified slick slider with products at the home page

--- HomePage/views.py
+++ HomePage/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from Products.models import Product
+from Products.models import Product, ProductCategory
 from .models import AboutUs, RecommendedProductsPhotos
 
 
 def home_page(request):
     home_info = AboutUs.objects.all()
 
-    top_sell = Product.objects.all().order_by('?').select_related('main_category', 'subcategory')
-    top_sell_sec = Product.objects.all().order_by('-id').select_related('main_category', 'subcategory')
+    top_sell = Product.objects.all().order_by('?').select_related('main_category', 'subcategory').exclude(
+        main_category_id=7).exclude(main_category_id=8).exclude(main_category_id=9)
+    top_sell_sec = Product.objects.all().order_by('-id').select_related('main_category', 'subcategory').exclude(
+        main_category_id=7).exclude(main_category_id=8).exclude(main_category_id=9)
+
+    bu_apple = Product.objects.filter(main_category__name='Apple б/у').order_by('?')
+    bu_apple_sec = Product.objects.filter(main_category__name='Apple б/у').order_by('-id')
 
     recommend_prod = RecommendedProductsPhotos.objects.all()
     context = {
@@ -15,5 +20,7 @@ def home_page(request):
         'recommend_prod': recommend_prod,
         'top_sell': top_sell,
         'top_sell_sec': top_sell_sec,
+        'bu_apple': bu_apple,
+        'bu_apple_sec': bu_apple_sec,
     }
     return render(request, template_name='HomePage/home_page.html', context=context)
