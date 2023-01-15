9c68064acdafa0fc023baf9800ae7e0ec0e26ce3
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-01-15 02:44:35 +0200

    Updated products cards and slick slider

--- HomePage/views.py
+++ HomePage/views.py
@@ -8,8 +8,6 @@ def home_page(request):
 
     top_sell = Product.objects.all().order_by('?').select_related('main_category', 'subcategory').exclude(
         main_category_id=7).exclude(main_category_id=8).exclude(main_category_id=9)
-    top_sell_sec = Product.objects.all().order_by('-id').select_related('main_category', 'subcategory').exclude(
-        main_category_id=7).exclude(main_category_id=8).exclude(main_category_id=9)
 
     accessories = Product.objects.filter(main_category_id=8)
 
@@ -20,7 +18,6 @@ def home_page(request):
         'home_info': home_info,
         'recommend_prod': recommend_prod,
         'top_sell': top_sell,
-        'top_sell_sec': top_sell_sec,
         'accessories': accessories,
         'gadgets': gadgets,
     }
