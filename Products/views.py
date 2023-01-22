913e1d9130a76ec72f6a5e40a1fa9809fa1500a4
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-01-22 20:29:54 +0200

    Some not important updates

--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .utils import *
 from .forms import ReviewsForms, OrderingChoices
@@ -14,21 +14,16 @@ def search(request):
 
     if search_query:
         """If we have search_query, we pass the search data to the paginator"""
-
         products = get_search(search_query)
-
         paginator = Paginator(products, 12)
         page_num = request.GET.get('page', 1)
         page_objects = paginator.get_page(page_num)
     else:
         """If we dont have search_query, we are getting all products in database and pass data to the paginator"""
-
         products = Product.objects.all().select_related('main_category', 'subcategory')
-
         paginator = Paginator(products, 12)
         page_num = request.GET.get('page', 1)
         page_objects = paginator.get_page(page_num)
-
     return render(request, 'Products/products_list.html', {'page_obj': page_objects})
 
 
@@ -38,7 +33,6 @@ def all_products_list(request):
     products = Product.objects.all().select_related('main_category', 'subcategory')
 
     all_sort_form = OrderingChoices(request.POST or {'form': request.session.get('all_sort')})
-
     """Creating the key of sort in the session and passing the data from the form"""
     if request.method == 'POST':
         if all_sort_form.is_valid():
@@ -53,7 +47,6 @@ def all_products_list(request):
         products = products.order_by('-price_with_discount')
 
     """Passing the received data to the paginator"""
-
     paginator = Paginator(products, 12)
     page_num = request.GET.get('page', 1)
     page_objects = paginator.get_page(page_num)
@@ -62,7 +55,6 @@ def all_products_list(request):
         'page_obj': page_objects,
         'sort_form': all_sort_form,
     }
-
     return render(request, template_name='Products/products_list.html', context=context)
 
 
@@ -76,7 +68,6 @@ def products_by_category(request, category_id, slug):
                                                      'subcategory')
 
     cat_sort_form = OrderingChoices(request.POST or {'form': request.session.get('cat_sort')})
-
     """Creating the key of sort in the session and passing the data from the form"""
     if request.method == 'POST':
         if cat_sort_form.is_valid():
@@ -90,10 +81,7 @@ def products_by_category(request, category_id, slug):
     elif cat_sort == 'expensive':
         products = products.order_by('-price_with_discount')
 
-    # filtering products by category_id which = category primary key
-
     """Passing the received data to the paginator"""
-
     paginator = Paginator(products, 10)
     page_num = request.GET.get('page', 1)
     page_objects = paginator.get_page(page_num)
@@ -110,11 +98,7 @@ def products_by_category(request, category_id, slug):
 def products_by_subcategory(request, category_id, slug, sub_id):
     """Getting products by subcategory"""
 
-    category = ProductCategory.objects.get(pk=category_id)  # getting category primary key
-
     subcategory = ProductSubCategory.objects.get(pk=sub_id)  # getting subcategory primary key
-    subcategory_slug = ProductSubCategory.objects.get(slug=slug)  # getting subcategory slug
-
     products = Product.objects.filter(
         subcategory_id=sub_id,
         main_category_id=category_id).select_related('main_category',
@@ -122,7 +106,6 @@ def products_by_subcategory(request, category_id, slug, sub_id):
     # and category_id which = main_category_id
 
     sub_sort_form = OrderingChoices(request.POST or {'form': request.session.get('sub_sort')})
-
     """Creating the key of sort in the session and passing the data from the form"""
     if request.method == 'POST':
         if sub_sort_form.is_valid():
@@ -137,7 +120,6 @@ def products_by_subcategory(request, category_id, slug, sub_id):
         products = products.order_by('-price_with_discount')
 
     """Passing the received data to the paginator"""
-
     paginator = Paginator(products, 10)
     page_num = request.GET.get('page', 1)
     page_objects = paginator.get_page(page_num)
@@ -155,34 +137,23 @@ def products_detail(request, pk, category, subcategory, slug):
     """Getting product_detail"""
 
     product = Product.objects.get(pk=pk)  # getting product by primary_key
-    product_slug = Product.objects.get(slug=slug)  # getting product slug
-    category = ProductCategory.objects.get(slug=category)  # getting category_slug
-    subcategory = ProductSubCategory.objects.get(slug=subcategory)  # getting subcategory_slug
-
     color = ProductColorChoice.objects.filter(category=product.subcategory,
                                               memory=product.product_memory, version=product.product_version,
                                               is_active=True).select_related('product__main_category',
                                                                              'product__subcategory')
-
     memory = ProductMemoryChoice.objects.filter(category=product.subcategory, color=product.product_color,
                                                 version=product.product_version, is_active=True).select_related(
         'product__main_category',
         'product__subcategory').order_by('product__product_memory__int_memory_value')
-
     version = ProductVersionChoice.objects.filter(category=product.subcategory, color=product.product_color,
                                                   memory=product.product_memory, is_active=True).select_related(
         'product__main_category',
         'product__subcategory').order_by('-id')
-
     photos = ProductPhotos.objects.filter(product=product)
-
     reviews = Reviews.objects.filter(product=product)  # filtering reviews by product
-
-    # recommended_products = Product.objects.filter(main_category=category, subcategory=subcategory)
     recommended_products = Product.objects.all().order_by('?').select_related('main_category', 'subcategory')
 
     """Posting reviews and getting them"""
-
     if request.method == 'POST':
         form = ReviewsForms(request.POST)
         if form.is_valid():
@@ -193,7 +164,6 @@ def products_detail(request, pk, category, subcategory, slug):
             data.ip = request.META.get('REMOTE_ADDR')
             data.product = product
             data.save()
-
             return HttpResponseRedirect(request.path)  # refresh the page to clear the form
     else:
         form = ReviewsForms()  # form output without data
